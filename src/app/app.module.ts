3eb6ff8edfa7ccbb8ab5debc55e384ca16ae5dd3
julio gomez <j.gomez@saas-ag.com>
2020-05-23 14:23:50 +0200

    First structure of the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './commons/header/header.component';
+import { FooterComponent } from './commons/footer/footer.component';
+import { FundamentalNgxCoreModule, ShellbarModule } from '@fundamental-ngx/core';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FundamentalNgxCoreModule,
+    ShellbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
